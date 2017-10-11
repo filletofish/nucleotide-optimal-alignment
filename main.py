51e110b6ba19ca1a2fe4c792aaf4c3012c3438db
Filipp Fediakov <julik103@mail.ru>
2017-10-11 11:20:14 +0300

    Place code for other algs

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from Source.scroring_functions import blosum_62_scoring
 from Source.smith_waterman import smith_waterman
+from Source.hirschberg import hirschberg
+from Source.needleman_wunsch import needleman_wunsch
 
 
 def main():
@@ -12,7 +14,8 @@ def main():
     chain_a_raw, chain_b_raw = str.split("\n")
     chain_a = np.array(list(chain_a_raw))
     chain_b = np.array(list(chain_b_raw))
-
+    # alignments = needleman_wunsch(chain_a, chain_b, gap_penalty=-5, similarity_func=blosum_62_scoring)
+    # alignments = hirschberg(chain_a, chain_b, gap_penalty=-5, similarity_func=blosum_62_scoring)
     alignments = smith_waterman(chain_a, chain_b, gap_penalty=-5, similarity_func=blosum_62_scoring)
     print("Result\n", alignments[0], "\n", alignments[1])
     f.close()
