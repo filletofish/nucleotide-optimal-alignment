a2a6504b1f457ba8e7faf78be5ff008c837517f1
Filipp Fediakov <julik103@mail.ru>
2017-12-20 18:01:22 +0300

    Enhance output, add final test input

--- Source/input_output.py
+++ Source/input_output.py
@@ -4,34 +4,37 @@ from io import StringIO
 from Source.NucleotideChain import NucleotideChain
 
 
+min_line_length = 20
+
+
 def read_input(data):
     records = SeqIO.parse(StringIO(data), "fasta")
     return [NucleotideChain(record.id, np.array(record.seq)) for record in records]
 
 
 def print_output(name_chain_a, name_chain_b, aligned_chain_seq_a, aligned_chain_seq_b, score):
-    if score is not None:
-        print("Score: ", score)
+    while len(name_chain_a) > min_line_length and len(name_chain_b) > min_line_length:
+        print("1: " + name_chain_a[:min_line_length])
+        print("2: " + name_chain_b[:min_line_length])
 
-    while len(name_chain_a) > 10 and len(name_chain_b) > 10:
-        print("1: " + name_chain_a[:10])
-        print("2: " + name_chain_b[:10])
-
-        name_chain_a = name_chain_a[10:]
-        name_chain_b = name_chain_b[10:]
+        name_chain_a = name_chain_a[min_line_length:]
+        name_chain_b = name_chain_b[min_line_length:]
     print("1: " + name_chain_a)
     print("2: " + name_chain_b)
 
     print("\nAligned:\n")
 
-    while len(aligned_chain_seq_a) > 10 and len(aligned_chain_seq_b) > 10:
-        print("1: " + aligned_chain_seq_a[:10])
-        print("2: " + aligned_chain_seq_b[:10])
-        aligned_chain_seq_a = aligned_chain_seq_a[10:]
-        aligned_chain_seq_b = aligned_chain_seq_b[10:]
+    while len(aligned_chain_seq_a) > min_line_length and len(aligned_chain_seq_b) > min_line_length:
+        print("1: " + aligned_chain_seq_a[:min_line_length])
+        print("2: " + aligned_chain_seq_b[:min_line_length])
+        aligned_chain_seq_a = aligned_chain_seq_a[min_line_length:]
+        aligned_chain_seq_b = aligned_chain_seq_b[min_line_length:]
         print("")
     print("1: " + aligned_chain_seq_a)
     print("2: " + aligned_chain_seq_b)
+
+    if score is not None:
+        print("\nScore: ", score)
 
 
 def print_output_multi(chains, chains_aligned):
